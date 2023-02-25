f43c301024f7b89b29f3cdc28fc7bc23029cf0f4
abdulhaq abdullah <haqabdullahov@gmail.com>
2023-02-25 15:38:46 +0600

    set pundit on animal, everything works

---
+++ app/policies/animal_policy.rb
@@ -0,0 +1,38 @@
+class AnimalPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    return true
+  end
+
+  def show?
+    return true
+  end
+
+  def new?
+    return create?
+  end
+
+  def create?
+    return true
+  end
+
+  def edit?
+    return update?
+  end
+
+  def update?
+    return record.user == user
+  end
+
+  def destroy?
+    return record.user == user
+  end
+
+
+end
